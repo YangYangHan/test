c4f25d3272c0ee9bdef94588ae59abc022067e24
HanYang <1017244101@qq.com>
2017-07-17 23:42:40 +0800

    7-17

---
+++ 2017-7-14/1.js
@@ -0,0 +1,19 @@
+var div = document.getElementById('div');
+div.addEventListener('mousedown',function(ev){
+	var disX = ev.clientX - this.offsetLeft;
+	var disY = ev.clientY - this.offsetTop;
+	
+	document.addEventListener('mousemove',move);
+	document.addEventListener('mouseup',up);
+	function up(){
+		document.removeEventListener('mousemove',move);
+		document.removeEventListener('mouseup',up);
+	}
+	function move(ev){
+		div.style.left = ev.clientX - disX + 'px';
+		div.style.top = ev.clientY - disY + 'px';
+	}
+	ev.preventDefault();
+});
+
+
